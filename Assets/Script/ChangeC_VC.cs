dc9f05dd331370bfcad64c0316fb9cff31a9cb3a
Bhavana vembadi <bhavanavembadi3@gmail.com>
2024-02-15 12:40:02 +0530

    changed

--- Assets/Script/ChangeC_VC.cs
+++ Assets/Script/ChangeC_VC.cs
@@ -1,76 +1,73 @@
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChangeC_VC : MonoBehaviour
 {
-    /// List to hold players
-    public List<Player> players = new List<Player>();
-
-    // Sprites for players
-    public Sprite[] playerSprites;
-
-
-    // Timer for sprite change interval
-    public float spriteChangeInterval = 2f;
-    private float timer;
+    public TextMeshProUGUI t1;
+    public GameObject image;
+    public TextMeshProUGUI t2;
+    public GameObject image2;
+    public TextMeshProUGUI t3;
+    public GameObject image3;
+    public TextMeshProUGUI t4;
+    public GameObject image4;
+    public TextMeshProUGUI t5;
+    public GameObject image5;
+    public TextMeshProUGUI t6;
+    public GameObject image6;
+    public TextMeshProUGUI t7;
+    public GameObject image7;
+    public TextMeshProUGUI t8;
+    public GameObject image8;
+    public TextMeshProUGUI t9;
+    public GameObject image9;
+    public TextMeshProUGUI t10;
+    public GameObject image10;
+    public TextMeshProUGUI t11;
+    public GameObject image11;
+    public string[] randomTexts; // Array to hold your random texts
 
     void Start()
     {
-        // Create players and assign sprites
-        for (int i = 0; i < 11; i++)
-        {
-            int spriteIndex = Random.Range(0, playerSprites.Length); // Randomly select a sprite index
-            Player player = new Player(spriteIndex);
-            players.Add(player);
-        }
-
-        // Start the timer
-        timer = spriteChangeInterval;
+        // Call function to change text when the game starts
+        ChangeTextRandomly();
     }
 
-    void Update()
+    public void ChangeTextRandomly()
     {
-        // Update timer
-        timer -= Time.deltaTime;
+        image.SetActive(true);
+        image2.SetActive(true);
+        image3.SetActive(true);
+        image4.SetActive(true);
+        image5.SetActive(true);
+        image6.SetActive(true);
+        image7.SetActive(true);
+        image8.SetActive(true);
+        image9.SetActive(true);
+        image10.SetActive(true);
+        image11.SetActive(true);
 
-        // If timer reaches zero, change sprites
-        if (timer <= 0f)
-        {
-            ChangeSprites();
-            timer = spriteChangeInterval;
-        }
-    }
 
-    // Method to randomly change sprites of all players
-    public void ChangeSprites()
-    {
-        foreach (Player player in players)
+        // Generate a random index within the range of the array
+
+        int randomIndex = Random.Range(0, randomTexts.Length);
+        /*if (randomTexts == randomTexts)
         {
-            int spriteIndex = Random.Range(0, playerSprites.Length); // Randomly select a new sprite index
-            player.ChangeSprite(spriteIndex);
-        }
+
+        }*/
+
+        // Set the text to a random text from the array
+        t1.text = randomTexts[randomIndex];
+        t2.text = randomTexts[randomIndex];
+        t3.text = randomTexts[randomIndex];
+        t4.text = randomTexts[randomIndex];
+        t5.text = randomTexts[randomIndex];
+        t6.text = randomTexts[randomIndex];
+        t7.text = randomTexts[randomIndex];
+        t8.text = randomTexts[randomIndex];
+        t9.text = randomTexts[randomIndex];
+        t10.text = randomTexts[randomIndex];
+        t11.text = randomTexts[randomIndex];
+
     }
 }
-
-public class Player
-{
-    // Index of sprite
-    public int spriteIndex;
-
-    // Constructor
-    public Player(int index)
-    {
-        spriteIndex = index;
-    }
-
-    // Method to change the sprite
-    public void ChangeSprite(int newIndex)
-    {
-        spriteIndex = newIndex;
-        // Apply changes to the sprite here (e.g., update sprite renderer)
-        Debug.Log("Player Sprite Index Changed to: " + spriteIndex);
-    }
-}
-
-
-
